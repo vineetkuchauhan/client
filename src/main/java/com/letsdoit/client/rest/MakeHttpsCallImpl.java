629dd613bc05ab40b6773cc293cd5ada0b797575
vineet kumar <vineetkuchauhan@gmail.com>
2019-06-17 05:18:19 +0530

    first commit

---
+++ src/main/java/com/letsdoit/client/rest/MakeHttpsCallImpl.java
@@ -0,0 +1,25 @@
+package com.letsdoit.client.rest;
+
+import com.letsdoit.client.services.MakeHttpsCall;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MakeHttpsCallImpl implements MakeHttpsCall {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public String makeHttpsCall() {
+        return restTemplate.getForObject("https://localhost:8085/onewayssl/v1/test",String.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+}
